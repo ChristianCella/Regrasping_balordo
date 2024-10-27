49cf6ec6e7ce61d439bab730735fa8c9dca83274
ChristianCella <christian.cella@polimi.it>
2024-10-27 14:51:14 +0100

    Updated the main.py script

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# --------------- Simple script for the detection stage
+
+# Import the YOLO class from the ultralytics module
 from ultralytics import YOLO
 
-# Load the model
+# Load the model and the weights
 model = YOLO('yolo11n.pt')
 
-# Perform object detection on an image and specify save directory
+# Perform object detection on an image 
 results = model("/home/christian/projects/Regrasping_balordo/Images/Bottle1.png")
 results[0].show()
