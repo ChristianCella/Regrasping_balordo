07786b6053dded013c389801bcbc8083c7bdc828
Matteo-Colombo <matteo2.colombo@polimi.it>
2024-10-31 17:28:51 +0100

    Aded bbox extraction

--- YOLO_simple_demo.py
+++ YOLO_simple_demo.py
@@ -1,10 +1,31 @@
 from pathlib import Path
 from ultralytics import YOLO
 
-# Load the model and the weights fro segmentation
-model = YOLO("yolo11n-seg.pt")
-
-# Perform object detection on an image 
 image_path = Path("Images/Bottle1.png")
-results = model(image_path)
-results[0].show()
+
+def YOLO_segment(image_path):
+    # Load the model and the weights fro segmentation
+    model = YOLO("yolo11n-seg.pt")
+
+    # Perform object detection on an image 
+    results = model(image_path)
+    results[0].show()
+    return results
+
+def YOLO_detect(image_path):
+    # Load the model and the weights fro segmentation
+    model = YOLO('yolo11n.pt')
+
+    # Perform object detection on an image for class 39, bottle
+    results = model.predict(image_path, classes=[39])
+    results[0].show()
+    return results
+
+# main to execute the function
+if __name__ == "__main__":
+    pred = YOLO_detect(image_path)
+
+    for r in pred:
+        print(r.boxes.cls)
+        print(r.boxes)
+        
