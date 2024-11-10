b4e302f07b5090a7201c66c00dcb526ef928db99
ChristianCella <christian.cella@polimi.it>
2024-11-10 18:47:30 +0100

    Added a file specific for detection

--- Old_main.py
+++ Old_main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 # Load the model and the weights
 model = YOLO('yolo11n.pt')
 
-image_path = Path("Images\Bottle1.png")
+image_path = Path("Images/Bottle1.png")
 
 # Perform object detection on an image 
 results = model(image_path)
