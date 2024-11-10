6584afbfdcc424d12aed1332515e45e36d98b807
ChristianCella <christian.cella@polimi.it>
2024-11-10 17:57:59 +0100

    Chenged the branch dev_detection

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 
 # Import the YOLO class from the ultralytics module
 from ultralytics import YOLO
+from pathlib import Path
 
 # Load the model and the weights
 model = YOLO('yolo11n.pt')
 
+image_path = Path("Images\Bottle1.png")
+
 # Perform object detection on an image 
-results = model("/home/christian/projects/Regrasping_balordo/Images/Bottle1.png")
+results = model(image_path)
 results[0].show()
