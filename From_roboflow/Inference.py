426119574315bf4ea3f14efe89e79d752931fb73
ChristianCella <christian.cella@polimi.it>
2024-11-18 09:42:50 +0100

    Modified the file for the detection

--- From_roboflow/Inference.py
+++ From_roboflow/Inference.py
@@ -3,7 +3,7 @@
 from ultralytics import YOLO
 from pathlib import Path
 
-model = YOLO("C:/Users/chris/OneDrive - Politecnico di Milano/Politecnico di Milano/PhD - dottorato/GitHub repositories Lenovo/Regrasping_balordo/runs/detect/train9/weights/best.pt")
-
-image_path = Path("Train-bottle-detection-1/valid/images/aug_0_1283_png.rf.6cb95b8aae4bca5aecbff77adb875050.jpg")
+#model = YOLO("C:/Users/chris/OneDrive - Politecnico di Milano/Politecnico di Milano/PhD - dottorato/GitHub repositories Lenovo/Regrasping_balordo/runs/detect/train9/weights/best.pt")
+model = YOLO(str(Path("runs/detect/train9/weights/best.pt")))
+image_path = Path("Images_on_slider/Bottle11.png")
 results = model.predict(str(image_path), imgsz = 640, conf = 0.2, save = True)
