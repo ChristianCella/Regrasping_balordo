845a3ab1a64e73c5ca7fe572626feb7e64ca7533
ChristianCella <christian.cella@polimi.it>
2024-11-27 16:20:16 +0100

    Retraining for the becker

--- Roboflow_interface/Inference.py
+++ Roboflow_interface/Inference.py
@@ -5,5 +5,5 @@ from pathlib import Path
 
 
 model = YOLO(str(Path("runs/detect/train/weights/best.pt")))
-image_path = Path("Images/Random_validation_images/Bottle11.png")
-results = model.predict(str(image_path), imgsz = 640, conf = 0.01, save = True) 
+image_path = Path("Images/Random_validation_images/20241127_135349.jpg")
+results = model.predict(str(image_path), imgsz = 640, conf = 0.6, save = True) 
