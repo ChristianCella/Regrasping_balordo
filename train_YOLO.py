7fbf4c4975506d719804790f08e23431811cfa3f
taffi <taffi98.at@gmail.com>
2024-11-29 19:52:32 +0100

    eorking yolo

--- train_YOLO.py
+++ train_YOLO.py
@@ -6,18 +6,13 @@ if __name__ == '__main__':
     print("CUDA available:", torch.cuda.is_available())
     print("Device:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU detected")
 
-
-
-    #model = YOLO("/Project/Regrasping_balordo_taffi/weights_christian/best.pt")
-    # accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
-    #results = model.predict(source="/Project/Regrasping_balordo_taffi/data/images/bins/flash", show=True,save=True) 
-
     # Create an instance of the YOLO model
-    # model = YOLO("yolo11n.pt")
-    # results = model.train(data = str(Path("/Project/Regrasping_balordo_taffi/data/dataset_ready_YOLO/data.yaml")), epochs = 100, imgsz = 640, workers = 8, batch = 32, device = 0)
+    #model = YOLO("yolo11n.pt")
+    #results = model.train(data = str(Path("/Project/Regrasping_balordo/data/dataset_ready_YOLO/data.yaml")), epochs = 300, imgsz = 640, workers = 8, batch = 4, device = 0)
 
     # # Evaluate model performance on the validation set
-    # metrics = model.val()
+    #metrics = model.val()
 
     # # Perform object detection on an image
-    # model.predict("/Project/Regrasping_balordo_taffi/data/images/6_items/", show=True)
+    model = YOLO("/runs/detect/train3/weights/best.pt")
+    model.predict("/Project/Regrasping_balordo/data/images/bins/flash", save=True)
