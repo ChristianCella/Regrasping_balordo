4eb265bd5c70b78b683547b0b097de586b7a90cf
ChristianCella <christian.cella@polimi.it>
2024-12-02 18:47:52 +0100

    re-ordered the repository and trained on the Dell laptop

--- Roboflow_interface/Inference.py
+++ Roboflow_interface/Inference.py
@@ -2,8 +2,28 @@
 
 from ultralytics import YOLO
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-
-model = YOLO(str(Path("runs/detect/train/weights/best.pt")))
+model = YOLO(str(Path("Training/train/weights/best.pt")))
 image_path = Path("Images/Random_validation_images/20241127_135349.jpg")
-results = model.predict(str(image_path), imgsz = 640, conf = 0.6, save = True) 
+save_directory = Path("Images/Detection_results")
+
+# Inference
+results = model.predict(
+    str(image_path), 
+    imgsz = 640, 
+    conf = 0.6, 
+    save = False) 
+
+# Result of the detection
+detected_image = results[0].plot()
+
+# Save the image
+save_path = save_directory / f"{image_path.stem}_predictions.jpg"
+plt.imsave(str(save_path), detected_image)
+
+# Display the image
+plt.imshow(detected_image)
+plt.axis('off')  # Turn off axis for better visualization
+plt.title("Detected becker")
+plt.show()
--- Roboflow_interface/Train_model.py
+++ Roboflow_interface/Train_model.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 from pathlib import Path
 import torch
 
-verbose = False
+verbose = True
 
 if __name__ == '__main__':
 
@@ -17,6 +17,20 @@ if __name__ == '__main__':
         print("CUDA available:", torch.cuda.is_available())
         print("Device:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU detected")
 
-    # Create an instance of the YOLO model
+    # Create an instance of the YOLO model and specify a custom directory
     model = YOLO("yolov8n.pt")
-    results = model.train(data = str(Path("Images\Labelled\data.yaml")), epochs = 100, imgsz = 640, workers = 8, batch = 32, device = 0)
+    custom_dir = Path("Training")
+
+    # In case of ubuntu (GPU GTX 1650 Ti => muc smaller computational capability)
+    results = model.train(
+        data = str(Path("Images/Labelled/data.yaml")), 
+        epochs = 150, 
+        imgsz = 512, 
+        workers = 1, 
+        batch = 4, 
+        device = 0, 
+        lr0=1e-4,
+        project = str(custom_dir))
+
+    # In case of windows (GPU RTX 4070 ==> much bigger computational capability)    
+    #results = model.train(data = str(Path("Images/Labelled/data.yaml")), epochs = 100, imgsz = 640, workers = 8, batch = 32, device = 0, lr0=1e-4, project = str(custom_dir))
