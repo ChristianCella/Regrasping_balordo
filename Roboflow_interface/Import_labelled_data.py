9c57f853d8b97941f3e67e5de406b412dcd1cf05
ChristianCella <christian.cella@polimi.it>
2024-12-10 19:20:26 +0100

    Temp

--- Roboflow_interface/Import_labelled_data.py
+++
@@ -1,7 +0,0 @@
-# You need to have roboflow installed in your virtual environment
-
-from roboflow import Roboflow
-rf = Roboflow(api_key="ieuVIgoSLJvfjf8y5cco")
-project = rf.workspace("christiancella22").project("wara_bottles_detection")
-version = project.version(1)
-dataset = version.download("yolov8")
